0115dbde8e8cb06f9d020f40978bac3d27b75a3f
xinghunm <xinghunm@keyayun.com>
2023-03-16 20:16:16 +0800

    update script

---
+++ scripts/build-bundle-and-upload.sh
@@ -0,0 +1,35 @@
+set -e
+# yarn buildBundle
+baylRepoName=bayl-service
+branch=test
+
+uploadBundleToBayl(){
+  yarn 
+  cd $baylRepoName
+  git pull
+  cp -r ../dist/ third_party/generate_pdf/
+  git status
+  git add .
+  git commit -m "update script"
+  git push origin $branch
+}
+
+buildAndUpdate(){
+  yarn buildBundle
+  git status
+  git add .
+  git commit -m "update script"
+  git push
+}
+
+buildAndUpload(){
+  buildAndUpdate
+  # uploadBundleToBayl
+}
+
+if [ -d $baylRepoName ]; then
+  buildAndUpload
+else
+  git clone -b test git@git.keyayun.com:keyayun/bayl-service.git
+  buildAndUpload
+fi
