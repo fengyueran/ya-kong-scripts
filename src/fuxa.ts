c9c4f7d9bc87ac5022b232d7e256e6138f1ba6bb
xinghunm <xinghunm@curacloudcorp.com>
2023-08-31 16:03:01 +0800

    add  script to create fuxa device config

---
+++ src/fuxa.ts
@@ -0,0 +1,31 @@
+import fs from "fs/promises";
+import fse from "fs-extra";
+import { v4 } from "uuid";
+
+const start = async () => {
+  const [, , deviceConfigFile] = process.argv;
+  const fuxaDeviceConfig: any = await fse.readJson(deviceConfigFile);
+
+  for (let i = 0; i < 1000; i += 1) {
+    const key = `t_${v4().slice(0, 8)}-${v4().slice(0, 8)}`;
+
+    fuxaDeviceConfig[1].tags[key] = {
+      id: key,
+      daq: {
+        enabled: false,
+        changed: true,
+        interval: 60,
+      },
+      name: `test${i + 1}`,
+      type: "Int16",
+      address: `${i + 1}`,
+      memaddress: "400000",
+      timestamp: 1693466381950,
+      value: null,
+    };
+  }
+
+  await fs.writeFile("output.json", JSON.stringify(fuxaDeviceConfig, null, 2));
+};
+
+start();
